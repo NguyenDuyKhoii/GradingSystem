d749ad951d6da174752a18ae2c360801b90709d6
NguyenDuyKhoii <khoind1235@gmail.com>
2026-07-24 14:07:42 +0700

    cleanup: remove old leftover unnested folders after microservices refactoring

--- be/FptuGradingSystem.API/Hubs/NotificationHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-namespace FptuGradingSystem.API.Hubs
-{
-    public class NotificationHub : Hub
-    {
-        // SignalR Hub class can be left empty for basic server-to-client push notifications.
-    }
-}
